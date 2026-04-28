c23c3aeb8fab9a3d472635e88e0b6f533579ea31
alkangunlu <alkangunlu@gmail.com>
2026-04-28 08:40:38 +0300

    fix: settings.gradle.kts pluginManagement plugins bloğu kaldırıldı
    - AGP/Kotlin/KSP versiyon tutarsızlığı çözüldü
    - settings.gradle.kts plugins versiyonları (Kotlin 2.0.21, AGP 9.1.0) ile
      root build.gradle.kts (Kotlin 2.2.10, AGP 9.1.1) arasında çakışma vardı
    - Artık root build.gradle.kts tek source of truth
    - Clean build ve testDebugUnitTest başarılı

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,13 +8,6 @@ pluginManagement {
         gradlePluginPortal()
     }
 
-    plugins {
-        id("com.android.application") version "9.1.0"
-        id("org.jetbrains.kotlin.android") version "2.0.21"
-        id("org.jetbrains.kotlin.plugin.compose") version "2.0.21"
-        id("com.google.devtools.ksp") version "2.0.21-1.0.28"
-        id("com.google.gms.google-services") version "4.4.2"
-    }
 }
 
 dependencyResolutionManagement {
